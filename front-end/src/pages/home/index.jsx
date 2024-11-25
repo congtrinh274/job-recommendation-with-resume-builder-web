52b0f8895ed81d01fc687f08bb3563f2b6a858f6
congit27 <conbo1102a@gmail.com>
2024-11-26 00:17:45 +0700

    add: pages

---
+++ front-end/src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import Header from '@/components/custom/Header';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            Content
+        </div>
+    );
+}
+
+export default Home;
