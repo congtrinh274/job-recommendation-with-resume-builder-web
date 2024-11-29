deb5b5859a32531af0722fce59343a96ab393588
congit27 <conbo1102a@gmail.com>
2024-11-30 00:39:37 +0700

    add: multer middleware

---
+++ back-end/src/middlewares/multer.middleware.js
@@ -0,0 +1,21 @@
+// Cấu hình Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
+// Middleware Multer
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'application/pdf') {
+            cb(null, true);
+        } else {
+            cb(new Error('Chỉ chấp nhận file PDF!'), false);
+        }
+    },
+});
