87109d622af079905afa145200b618aa460e8258
congit27 <conbo1102a@gmail.com>
2024-12-01 03:04:21 +0700

    update: User model

--- back-end/src/models/User.js
+++ back-end/src/models/User.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
     {
-        clerkUserId: { type: String, unique: true },
-        username: { type: String },
+        authId: { type: String, unique: true },
         email: { type: String, required: true, unique: true },
+        username: { type: String },
         password: { type: String },
-        role: { type: String, enum: ['candidate', 'recruiter', 'admin'], required: true },
+        typeLogin: { type: String },
+        loginToken: { type: String },
+        imgUrl: { type: String },
+        role: { type: String, enum: ['CANDIDATE', 'RECRUITER', 'ADMIN'], required: true, default: 'CANDIDATE' },
     },
     {
         timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
