0ebba0c86fb9f172ff20ef1e8e3459d936a12953
congit27 <conbo1102a@gmail.com>
2024-12-06 20:00:30 +0700

    update: auth controller > login success logic

--- back-end/src/controllers/AuthController.js
+++ back-end/src/controllers/AuthController.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 const User = require('../models/User');
 const { generateToken } = require('../utils/jwt.js');
+const { v4: uuidv4 } = require('uuid');
 
 class AuthController {
     // [POST] api/auth/login-success/
     loginSuccess = async (req, res) => {
-        const { id } = req?.body;
+        const { id, loginToken } = req?.body;
+        console.log(loginToken);
         try {
-            if (!id) {
+            if (!id || !loginToken) {
                 return res.status(400).json({
                     err: 1,
                     msg: 'Missing payloads',
                 });
             }
 
-            const user = await User.findOne({ authId: id });
+            const user = await User.findOne({ authId: id, loginToken: loginToken });
 
             if (!user) {
                 return res.status(404).json({
@@ -23,34 +25,16 @@ class AuthController {
                 });
             }
 
-            const token = await generateToken({ id: user.authId, email: user.email, role: user.role });
-
-            user.loginToken = token;
+            const newLoginToken = uuidv4();
+            user.loginToken = newLoginToken;
             await user.save();
+
+            const token = await generateToken({ id: user.authId, email: user.email, role: user.role });
 
             return res.status(200).json({
                 err: 0,
                 msg: 'Login successful',
                 token,
-            });
-        } catch (error) {
-            return res.status(500).json({
-                err: -1,
-                msg: 'Fail at AuthController: ' + error.message,
-            });
-        }
-    };
-
-    // [POST] api/auth/logout
-    logout = async (req, res) => {
-        try {
-            const userId = req.user.id;
-
-            await User.findOneAndUpdate({ authId: userId }, { loginToken: null });
-
-            return res.status(200).json({
-                err: 0,
-                msg: 'Logout successful',
             });
         } catch (error) {
             return res.status(500).json({
